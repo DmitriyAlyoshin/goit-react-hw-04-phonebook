bb61554e7d5e09d3f98492a73d13d1b72fd911b0
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-07-03 22:58:29 +0200

    Oy_vey_ver_3

---
+++ src/data/initialContacts.jsx
@@ -0,0 +1,9 @@
+export const initialContacts = [
+  { id: 'id-1', name: 'Eden Clements', number: '+380956450179' },
+  { id: 'id-2', name: 'Annie Copeland', number: '+380952279126' },
+  { id: 'id-3', name: 'Trevor Bangladesh', number: '+380959562876' },
+  { id: 'id-4', name: 'Rosie Simpson', number: '+380954591256' },
+  { id: 'id-5', name: 'Gabriel Gmurek', number: '+380950001488' },
+  { id: 'id-6', name: 'Hermione Kline', number: '+380954438912' },
+  { id: 'id-7', name: 'Abdul Makharadgi', number: '+380951271334' },
+];
